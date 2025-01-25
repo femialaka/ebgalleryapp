e229cb75b61a5ed2e6fa434bd85aeebe290396db
Femi Alaka <femialakadev@gmail.com>
2025-01-25 14:07:49 +0000

    update 04

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ console.log(process.env.RDS_PORT)
 // });
 
 app.get('/', (req, res) => {
-  res.send('Lets go baby! Its time to rock');
+  res.send('Lets go baby! Its time to rock, there is now');
 });
 
 
