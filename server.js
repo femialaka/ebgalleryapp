69008d44d5e53d034caec5096e6eab71493aa7f5
Femi Alaka <femialakadev@gmail.com>
2025-01-27 10:18:00 +0000

    added Postgres variables

--- server.js
+++ server.js
@@ -3,32 +3,22 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// const client = new Client({
-//   host     : process.env.RDS_HOSTNAME,
-//   user     : process.env.RDS_USERNAME,
-//   database : process.env.RDS_DB_NAME,
-//   password : process.env.RDS_PASSWORD,
-//   port     : process.env.RDS_PORT || 5432
-// });
+const client = new Client({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  database : process.env.RDS_DB_NAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT || 5432
+});
 
-// console.log(process.env.RDS_HOSTNAME)
-// console.log(process.env.RDS_USERNAME)
-// console.log(process.env.RDS_PASSWORD)
-// console.log(process.env.RDS_DB_NAME)
-// console.log(process.env.RDS_PORT)
-
-// client.connect(function(err) {
-//   if (!err) {
-//     console.log('Connected to database.');
-//   } else { 
-//     console.error('Database connection failed: ' + err.stack);
-//   }
-//   client.end;
-  
-// });
+console.log(process.env.RDS_HOSTNAME)
+console.log(process.env.RDS_USERNAME)
+console.log(process.env.RDS_PASSWORD)
+console.log(process.env.RDS_DB_NAME)
+console.log(process.env.RDS_PORT)
 
 app.get('/', (req, res) => {
-  res.send('Lets go baby! Its heck time to rock,come on helloo!!!');
+  res.send('Lets go baby! Its heck time to rock,come on db Update!!!');
 });
 
 
